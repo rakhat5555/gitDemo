83cebae777a4921537a1973d460ce9d35aa20ac9
rakhat5555 <rakhat5555@gmail.com>
2020-08-01 08:51:07 -0700

    added else if statement

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -2,8 +2,11 @@ public class HelloB20 {
     public static void main(String[] args) {
         System.out.println("Hello Rakhat");
         // add this code
-        if(3 == 3) {
-            System.out.println("It's true");
+        int year = 2020;
+        if(year % 4 == 0) {
+            System.out.println("It's a Leap year");
+        } else if (year % 100 == 0) {
+            System.out.println("IT's a leap year");
         }
     }
 }
