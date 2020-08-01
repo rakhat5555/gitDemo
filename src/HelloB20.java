198a400d19e501a3301d64b2a6d5b75f114ab1bb
rakhat5555 <rakhat5555@gmail.com>
2020-08-01 08:47:25 -0700

    added if statement

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -1,5 +1,9 @@
 public class HelloB20 {
     public static void main(String[] args) {
         System.out.println("Hello Rakhat");
+        // add this code
+        if(3 == 3) {
+            System.out.println("It's true");
+        }
     }
 }
